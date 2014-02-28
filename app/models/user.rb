ee973be4bcfcec5bef57246cded7a8ed76e4ef90
JesterMan <seanglang@gmail.com>
2014-02-28 00:05:58 -0800

    error while following steps on 42floors.com

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class Users < ActiveRecord::Base
+
+  attr_accessor :password
+  attr_protected :password_digest
+
+  validates :name, :presence => true
+  validates :email, :presence => true, :uniqueness => true, :email => true
+  validates :password, :presence => true, :confirmation => true
+  validates :password_confirmation, :presence => { :if => :password }
+  validates :phone, :format => { :allow_nil => true, :with => /^[()0-9- +.]{10,20}s*[extension.]{0,9}s*[0-9]{0,5}$/i }
+
+  def password=(pass)
+    return if pass.blank?
+    @password = pass
+    self.password_digest = BCrypt::Password.create(pass)
+  end
+
+end
